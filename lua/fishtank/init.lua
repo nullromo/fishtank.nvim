24146a9fc064fd70ed7fac5a5cce6ab3b6a18956
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2025-08-04 16:37:25 -0700

    add autocmd to pause while typing a command

--- lua/fishtank/init.lua
+++ lua/fishtank/init.lua
@@ -2,10 +2,19 @@ local internals = require('fishtank.internals')
 
 local M = {}
 
+local fishtankNamespace = vim.api.nvim_create_namespace('fishtank.nvim')
+
 M.setup = function()
     vim.api.nvim_create_user_command('Fishtank', function(opts)
         internals.fishtankUserCommand(opts.args)
-    end, { nargs = '+', desc = 'Start fishtank' })
+    end, { nargs = '+', desc = 'Start fishtank.nvim' })
+
+    vim.api.nvim_create_autocmd({ 'CmdlineEnter' }, {
+        callback = function()
+            vim.notify('cmdline entered')
+        end,
+        desc = 'pause fishtank.nvim while typing a command',
+    })
 end
 
 return M
